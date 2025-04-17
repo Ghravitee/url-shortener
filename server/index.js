44d20e92275f5b6c1578e311f38de6ae7df268a7
Ayodele Temitope <dyenamics228@gmail.com>
2025-04-17 23:14:30 +0100

    remove origin

--- server/index.js
+++ server/index.js
@@ -6,14 +6,7 @@ import cors from "cors";
 const app = express();
 const PORT = 5000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://url-shortener-orpin-eta.vercel.app",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
